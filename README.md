edf2d4fcb2272c98d2fc1c7d1d410a379876fce0
Galexy5 <giwrgos.alex@hotmail.com>
2020-05-31 19:17:46 +0300

    It needs some extras  but it's almost done and functional

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mastermind
+
+For The Odin Project curriculum
 Mastermind board game in ruby
+
+******
+You are playing against the computer and you can choose to be the CodeBreaker or the CodeMaker.
+The CodeMaker creates a  4 letter secret code.He can choose between 5 colors (duplicates allowed).
+
+******
+The CodeBreaker gets feedback after every guess. 
+"Red" means that there is a right color in the right place.
+"White" means that there is a color but in the wrong place.
+
+******
+CodeBreaker has 12 rounds to find the secret code. 
+
+
--- mastermind.rb
+++ mastermind.rb
@@ -10,22 +10,36 @@ class Mastermind
      @guess=""
      @close=nil
      @role=role
-     if @role=="cb"
+     if @role=="1" # CodeBreaker
        @code=""
        4.times{@code+=@@code_generator[rand(1..5)]}
-     elsif @role=="cm"
-       puts "Enter 4 letters for your secret code (g:green, o:orange, b:blue, y:yellow, p:pink)"
+       puts "Colors to choose:\ng=green, o=orange, b=blue, y=yellow, p=pink (format: gopy)"
+     elsif @role=="2" # CodeMaker
+       print "g=green, o=orange, b=blue, y=yellow, p=pink\nEnter 4 letters for your secret code (format: gopy) : "
        @code=gets.chomp
-       while @code.length!=4 do
-             puts "Your secret code has to be 4 letters"
+       @valid=0
+       @code.each_char {|x|        # Code must include only the given colors
+        @@code_generator.each {|k,v| 
+        @valid+=1 if x==v 
+        } 
+       } 
+       while @code.length!=4 || @valid!=4 do  # Code must include only the given colors and 4 letters
+            @valid=0
+             print "\nInvalid entry !! \nYour secret code has to be 4 letters and include only the given colors\n Try again: "
              @code=gets.chomp.to_s
+             @code.each_char {|x|        
+              @@code_generator.each {|k,v| 
+              @valid+=1 if x==v 
+              } 
+             } 
            end
      end
      game
    end
  
    def guess
-     if @role=="cm"  #Computer guess
+    @valid=0
+     if @role=="2"  #Computer guess
        if @close==nil
       4.times{@guess+=@@code_generator[rand(1..5)]}
        else
@@ -34,10 +48,21 @@ class Mastermind
        end
      else
        @guess=gets.chomp.to_s  # User guess
-         while @guess.length!=4 do
-             puts "Your guess has to be 4 letters"
-             @guess=gets.chomp.to_s
-           end
+       @guess.each_char {|x|        # Calculate valid so I can check if the guess has only the given colors (valid must be 4)
+        @@code_generator.each {|k,v| 
+        @valid+=1 if x==v 
+        } 
+       } 
+       while @guess.length!=4 || @valid!=4 do  # Guess must include only the given colors and 4 letters
+        @valid=0
+         print "\nInvalid entry !! \nYour guess has to be 4 letters and include only the given colors\n Try again: "
+         @guess=gets.chomp.to_s
+         @guess.each_char {|x|        
+          @@code_generator.each {|k,v| 
+          @valid+=1 if x==v 
+          } 
+         } 
+       end
    end
    return @guess
  end
@@ -48,20 +73,20 @@ class Mastermind
      rounds=1
      c=@code.split("")
      while rounds<=12 do
-         puts "Round: #{rounds}"
+         print "\nRound #{rounds}: "
          @guess=guess
-         puts "Your guess is: #{@guess}"
+         puts "\nYour guess is: #{@guess}"
            g=@guess.split("")
              if @guess==@code
-               puts "Congratulations you broke the code...#{@code}"
+               puts "\nCongratulations you broke the code...#{@code}"
                rounds=13
              else
                for i in 0..3 do
                  if @code.include?(g[i].to_s)
                    if g[i]==c[i]
-                     puts "red"
+                     puts "Red"
                    else
-                     puts "white"
+                     puts "White"
                      @close=g[i]
                    end
                  end
@@ -69,7 +94,7 @@ class Mastermind
                end
        
        rounds+=1
-       puts "I'm sorry you lost...The secret code was: #{@code}" if rounds==13
+       puts "\nI'm sorry you lost...The secret code was: #{@code}" if rounds==13
        @guess=""
        end
  
@@ -81,10 +106,10 @@ class Mastermind
  play_again=true
  answer=""
  while play_again
-   puts "Press 'cb' for CodeBreaker ot 'cm' for CodeMaker"
-   role=gets.chomp
-   game=Mastermind.new(role)
-   puts "You want to play again ? y/n"
+   puts "\nChoose the role you want:\n 1) CodeBreaker \n 2) CodeMaker"
+   role=gets.chomp.to_s
+   game=Mastermind.new(role.downcase)
+   print "\nYou want to play again ? y/n: "
    answer=gets.chomp.downcase
    answer=="y" ? play_again=true : play_again=false
  end
